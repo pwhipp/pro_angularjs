2816f4fdd7a649a80a6bb08d3418d63e86849381
pwhipp <paul.whipp@gmail.com>
2014-09-28 17:12:44 +1000

    Added initial todo placeholder html (2.1)

--- core/settings.py
+++ core/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
+    'todo'
 )
 
 MIDDLEWARE_CLASSES = (
--- core/urls.py
+++ core/urls.py
@@ -3,8 +3,11 @@ from django.contrib import admin
 
 from django.views.generic import TemplateView
 
+import todo.urls
+
 urlpatterns = patterns(
     '',
+    url(r'todo/', include(todo.urls)),
     url(r'^$', TemplateView.as_view(template_name='core/index.html'), name='home'),
     url(r'^test/$', TemplateView.as_view(template_name='core/test.html'), name='test'),
     url(r'^admin/', include(admin.site.urls)))
