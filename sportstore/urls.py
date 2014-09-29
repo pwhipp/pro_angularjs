0585643bda6cd00957110362534813a7027f9320
pwhipp <paul.whipp@gmail.com>
2014-09-29 17:49:29 +1000

    Added angular-route.js and angular-resource.js to sportstore and set up appropriate initial template structure

--- sportstore/urls.py
+++ sportstore/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
 
 import sportstore.views as sv
 
 urlpatterns = patterns(
     '',
+    url(r'^$', TemplateView.as_view(template_name='sportstore/index.html'), name='sportstore_home'),
     url(r'products/$', sv.get_products, name='sportstore_products'))
