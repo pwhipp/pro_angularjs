4ffdbf95a19561082f274dadeb665695d1151996
pwhipp <paul.whipp@gmail.com>
2014-10-03 09:49:55 +1000

    Tweaked views to supply categories as names (meh) and simple products as json.

--- sportstore/views.py
+++ sportstore/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.http import HttpResponse
 from django.core import serializers
 
@@ -6,4 +8,15 @@ import sportstore.models as sm
 
 def get_products(request):
     products = serializers.serialize('json', sm.Product.objects.all())
+
+    # FTTB to conform with the book categories as strings (meh), convert our categories into the first category name.
+    def transform_product(p):
+        return dict(p['fields'].items() +
+                    [('id', p['pk']),
+                     ('category', sm.Category.objects.get(id=p['fields']['category']).name)])
+
+    products = json.loads(products)
+    products = [transform_product(product) for product in products]
+    products = json.dumps(products)
+
     return HttpResponse(products, content_type='application/json')
